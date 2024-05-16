6ae6442c3225a8b3cb5e170d44ff265afd1bc11c
valeen-rodriguez <valentinarodriguezkittler@gmail.com>
2024-05-16 02:24:14 -0300

    Carrito finalizado por completo

--- public/js/script.js
+++ public/js/script.js
@@ -102,19 +102,72 @@ try {
 //// BOTON BORRAR CARRITO ////
 btnC.addEventListener('click', async (e) => {
     try {
-        console.log('Iniciando solicitud para eliminar elementos del carrito...');
         const response = await fetch('/products/removeAllFromCart', {
             method: 'POST',
         });
-        console.log('Se recibio la respuesta', response);
+        console.log('Se recibio respuesta', response);
         if (response.ok) {
             console.log('Eliminando productos');
-            window.location.reload();
+            window.location.href = '/products/carrito';
         } else {
             console.error('Error eliminar del carrito:', response.statusText);
             alert('Hubo un error al eliminar los elementos del carrito');
         }
     } catch (error) {
         console.error('Error con los datos del carrito:', error);
+    }
+});
+
+// FINALIZAR COMPRA & VALIDACIONES //
+const btnConfirmarCompra = document.querySelector('.comprarFinalizada');
+
+btnConfirmarCompra.addEventListener('click', async (e) => {
+    const nombreCompleto = document.querySelector('input[name="nombre_completo"]').value;
+    const telefono = document.querySelector('input[name="telefono"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const direccion = document.querySelector('input[name="direccion"]').value;
+
+    if (!nombreCompleto || !telefono || !email || !direccion) {
+        Swal.fire({
+            icon: 'warning',
+            title: '¡Campos incompletos!',
+            text: 'Por favor, completa todos los campos para finalizar la compra.',
+            confirmButtonText: 'Aceptar'
+        });
+        return;
+    }
+
+    try {
+        const response = await fetch('/products/removeAllFromCart', {
+            method: 'POST',
+        });
+
+        if (response.ok) {
+            Swal.fire({
+                icon: 'success',
+                title: '¡Compra realizada!',
+                text: 'Sigue los pasos concisos enviados al email',
+                confirmButtonText: 'Aceptar',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = '/';
+                }
+            });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: '¡Error!',
+                text: 'Por favor, inténtalo de más tarde.',
+                confirmButtonText: 'Aceptar'
+            });
+        }
+    } catch (error) {
+        console.error('datos del carrito:', error);
+        Swal.fire({
+            icon: 'error',
+            title: '¡Error!',
+            text: 'Por favor, inténtalo de más tarde.',
+            confirmButtonText: 'Aceptar'
+        });
     }
 });
